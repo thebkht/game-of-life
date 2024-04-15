4e3bc34e21a732fcdff3b55084180f00d3f01a17
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-04-16 05:18:56 +0900

    Add new dependencies and update tailwind styles

---
+++ config /site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "bkhtdev/game of life",
+  url: "https://life.bkhtdev.com",
+  ogImage: "https://life.bkhtdev.com/og.jpg",
+  description:
+    "Conway's Game of Life is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input.",
+  links: {
+    twitter: "https://twitter.com/yusupovbg",
+    github: "https://github.com/yusupovbg/game-of-life",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
