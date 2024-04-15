29769e5111f930d64be250adc3ff5a3671ef26eb
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-04-16 05:31:37 +0900

    Update ogImage URL in siteConfig

--- config /site.ts
+++ config /site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "bkhtdev/game of life",
   url: "https://life.bkhtdev.com",
-  ogImage: "https://life.bkhtdev.com/og.jpg",
+  ogImage: "https://life.bkhtdev.com/og.png",
   description:
     "Conway's Game of Life is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input.",
   links: {
